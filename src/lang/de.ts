73cc205402be99b60a7a1fcf96572d37c8d2718e
Amer Harb <amer.harb@gmail.com>
2024-05-12 10:54:48 +0200

    240511 add finnish (#10)
    * add fi sound
    
    * add number text
    
    * refactor
    
    * add numbers file
    
    * add to ignore

---
+++ src/lang/de.ts
@@ -0,0 +1,19 @@
+import {Lang} from "./Lang";
+
+export const de: Lang = {
+	code: 'de',
+	display: 'German',
+	flag: '🇩🇪', numbers: [
+		'null',
+		'eins',
+		'zwei',
+		'drei',
+		'vier',
+		'fünf',
+		'sechs',
+		'sieben',
+		'acht',
+		'neun',
+		'zehn',
+	],
+}
---
+++ src/lang/en.ts
@@ -0,0 +1,20 @@
+import {Lang} from "./Lang";
+
+export const en: Lang = {
+	code: 'en',
+	display: 'English',
+	flag: '🇬🇧',
+	numbers: [
+		'zero',
+		'one',
+		'two',
+		'three',
+		'four',
+		'five',
+		'six',
+		'seven',
+		'eight',
+		'nine',
+		'ten',
+	],
+}
